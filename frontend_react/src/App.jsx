18d3f1c92e87da4827695e10e3fbc29b44beccef
seungmin lee <sorpwjdafh@naver.com>
2022-08-02 21:18:23 +0900

    commit

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,10 +1,12 @@
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 
+import "./App.scss";
+
 function App() {
   return (
     <div className="app">
-      <Navbar />
+      {/* <Navbar /> */}
       <Header />
       <About />
       <Work />
